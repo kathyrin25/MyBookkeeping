287c4e9a296b35360ad79c4bd911395320c5454b
kathy.peng <kathy.peng@KATHY-PENG-NB1>
2017-08-22 14:25:19 +0800

    利用Razor helper, 讓收入與支出以不同顏色顯示 / 加上資料驗證

---
+++ MyBookkeeping/Controllers/ValidateController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBookkeeping.Controllers
+{
+    public class ValidateController : Controller
+    {
+        public ActionResult CheckDate(string Date)
+        {
+            bool isValidate = false;
+            string ErrorMsg = String.Empty;
+
+            //if (Url.IsLocalUrl(Request.Url.AbsoluteUri))  //這個判斷式無法運作, 一樣都是localhost:，只是port不一樣
+            //{
+                //利用 IsLocalUrl檢查是否為網站呼叫的 , 借此忽略一些不必要的流量   
+                DateTime checkDate = new DateTime();
+                if (!DateTime.TryParse(Date, out checkDate))    //非正常的日期格式,加這段是因為IE前面擋不住不正常的日期
+                {
+                    ErrorMsg = "非日期格式，請檢查。";
+                }
+                else if (DateTime.Compare(checkDate, DateTime.Today) > 0)   //「日期」不得大於今天
+                {
+                    ErrorMsg = "日期不可大於今天。";
+                }
+                else
+                    isValidate = true;
+            //}           
+            
+
+            // Remote 驗證是使用 Get 因此要開放
+            if (isValidate)
+                return Json(isValidate, JsonRequestBehavior.AllowGet);
+            else
+                return Json(ErrorMsg, JsonRequestBehavior.AllowGet);  //回傳自訂的錯誤訊息
+        }
+
+
+     }
+}
--- MyBookkeeping/ViewModels/AddRecordViewModel.cs
+++ MyBookkeeping/ViewModels/AddRecordViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MyBookkeeping.ViewModels
 {
@@ -11,18 +12,26 @@ namespace MyBookkeeping.ViewModels
     {
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
         [Display(Name = "日期")]
+        [Required]
+        [RegularExpression(@"^([0-9]{4})[./]{1}([0-9]{2})[./]{1}([0-9]{2})$", ErrorMessage = "日期格式請輸入yyyy/MM/dd")]  
+        [Remote("CheckDate", "Validate")]
         public DateTime Date { get; set; }
 
+      
         [Display(Name = "類別")]
+        [Required]
         public BookType Type { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:N0}")]
         [Display(Name = "金額")]
-        public int Amount { get; set; }
+        [Required]
+        //[RegularExpression(@"^\+?[1-9][0-9]*$", ErrorMessage = "只能輸入大於0的正整數")] 
+        [RegularExpression(@"^\+?[0-9]*$", ErrorMessage = "只能輸入正整數")]  /*只能輸入正整數*/
+        public int Amount { get; set; }      
 
-        [Required(AllowEmptyStrings = true)]
-        [Display(Name = "備註")]
-        [StringLength(500)]
+        [Required]
+        [Display(Name = "備註")]       
+        [StringLength(100, ErrorMessage = "輸入字串長度不可超過{1}個字元")]
         public string Remark { get; set; }
     }
 }
