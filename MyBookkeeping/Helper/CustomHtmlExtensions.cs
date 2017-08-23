549c137a3e215237a4f603696509b1f087821671
kathy.peng <kathy.peng@KATHY-PENG-NB1>
2017-08-23 16:44:03 +0800

    收入與支出以不同顏色顯示改用另一種方式 : 自訂 Html Helper(Extension Methods Extension Methods)

---
+++ MyBookkeeping/Helper/CustomHtmlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MyBookkeeping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBookkeeping.Helper
+{
+    public static class CustomHtmlExtensions  /*一定要宣告為 static */
+    {
+        public static string ShowClass(this HtmlHelper helper, BookType _booktype)
+        {
+            return (_booktype == BookType.支出) ? "text-danger" : "text-primary";  //回傳Class name
+        }
+    }
+}
